39a2e02b25f55737b8d1c5e3336e00357f07c578
sun <skkimeo@gmail.com>
2021-09-26 17:37:37 +0900

    basic layout from lecture complete: cards in grid, tap actions

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,14 +8,84 @@
 import SwiftUI
 
 struct ContentView: View {
+    var vechicleEmojis = ["🚙", "🛴", "✈️", "🛵", "⛵️", "🚎", "🚐", "🚛",
+                          "🛻", "🏎", "🚂", "🚊", "🚀", "🚁", "🚢", "🛶",
+                          "🛥", "🚞", "🚤", "🚲", "🚡", "🚕", "🚟", "🚃"]
+    var foodEmojis = ["🍔"]
+    var aminalEmojis = ["🐶"]
+    
+    @State var emojiCount = 24
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(vechicleEmojis[0..<emojiCount], id: \.self) {
+                        CardView(content: $0).aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            .foregroundColor(.red)
+            Spacer()
+            HStack {
+                //chooseTheme(vechicleEmojis)
+                //chooseTheme(foodEmojis)
+                //chooseTheme(animalEmojis)
+            }
+        }
+        .padding(.horizontal)
     }
 }
 
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .preferredColorScheme(.light)
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
     }
 }
