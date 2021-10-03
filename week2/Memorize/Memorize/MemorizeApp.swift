dbb3c63f581e72dd8bc42656bb22d7387c634660
sun <skkimeo@gmail.com>
2021-10-03 16:27:24 +0900

    week 2 initial commit

---
+++ week2/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by sun on 2021/10/03.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
